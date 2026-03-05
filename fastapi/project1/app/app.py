d7abce6b2c902d23c4d48d51f5b10c5ee9b8a410
cringe-syco <gibertail10@gmail.com>
2026-03-05 13:08:16 +0530

    db configurations

--- fastapi/project1/app/app.py
+++ fastapi/project1/app/app.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate, PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
 
 app = FastAPI()
 
@@ -26,12 +36,19 @@ def get_all_posts(limit: int = None):
     return text_posts
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int) -> PostResponse:
     if id not in text_posts:
         raise HTTPException(status_code=404, detail = "Page not Found")
     return text_posts.get(id)
 
 
 @app.post("/posts")
-def create_post(title: str, content: str):
-    pass
+def create_post(post: PostCreate) -> PostResponse:
+    new_post = {
+        "title": post.title,
+        "contetnt": post.content
+    }
+    text_posts[max(text_posts.keys()) + 1]  = new_post
+    return new_post
+
+# @app.delete
