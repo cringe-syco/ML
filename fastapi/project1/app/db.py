c12896caa751a8b6d960ff0cb81e146302f92aaf
cringe-syco <gibertail10@gmail.com>
2026-03-11 11:48:48 +0530

    imagekit.io

--- fastapi/project1/app/db.py
+++ fastapi/project1/app/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
-import datetime
+from datetime import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
@@ -18,7 +18,7 @@ class Post(Base):
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.timezone.utc)
+    created_at = Column(DateTime, default=datetime.utcnow)
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker  = async_sessionmaker(engine, expire_on_commit=False)
@@ -29,4 +29,4 @@ async def create_db_and_tables():
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
-        yield session
+        yield session 
